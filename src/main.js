c716fe4cad93d5c0de1be21cb74714fc75c72668
AristoBudiman <aristobudimannew@gmail.com>
2026-06-12 23:25:48 +0700

    feat: Login Google

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
-import router from './router' 
+import router from './router'
+import './style.css'
+import vue3GoogleLogin from 'vue3-google-login'
 
 const app = createApp(App)
-
 app.use(router)
+app.use(vue3GoogleLogin, {
+  clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID
+})
 
 app.mount('#app')
